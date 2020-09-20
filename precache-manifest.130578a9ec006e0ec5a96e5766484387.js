a1192fde1ddfbf6003d83428524546de5ae4f5c6
Michael Hoha <michael1hoha@gmail.com>
2020-09-20 15:59:00 +0300

    Updates

---
+++ precache-manifest.130578a9ec006e0ec5a96e5766484387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4f4a4f2baa7549ca26dfc3d747906e",
+    "url": "/nameOfReposetory/index.html"
+  },
+  {
+    "revision": "c1c5a22d4e25cd5ef8da",
+    "url": "/nameOfReposetory/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "50f5723308c9012a5e97",
+    "url": "/nameOfReposetory/static/js/2.240e87d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nameOfReposetory/static/js/2.240e87d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c5a22d4e25cd5ef8da",
+    "url": "/nameOfReposetory/static/js/main.42117ca6.chunk.js"
+  },
+  {
+    "revision": "db9819c75c90bafe36d3",
+    "url": "/nameOfReposetory/static/js/runtime-main.d73cc22e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/nameOfReposetory/static/media/logo.ee7cd8ed.svg"
+  }
+]);
